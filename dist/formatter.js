785089425c9ef266d6d813b33e535b678ee7def0
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2020-03-13 12:20:03 +0100

    Added compiled version

---
+++ dist/formatter.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const command_1 = require("@actions/core/lib/command");
+module.exports = function (results) {
+    command_1.issue('group', 'ESLint Annotations');
+    for (const file of results.filter(r => r.messages.length > 0)) {
+        for (const message of file.messages) {
+            const msg = `${message.message} (${message.ruleId})`;
+            const severity = message.fatal || message.severity === 2 ? 'error' : 'warning';
+            const args = {
+                file: file.filePath.substr(process.cwd().length + 1),
+                line: message.line.toString(),
+                col: message.column.toString(),
+            };
+            command_1.issueCommand(severity, args, msg);
+        }
+    }
+    a;
+    command_1.issue('endgroup');
+    return '';
+};
